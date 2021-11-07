ece592ce5a7e5d063e68fad8a6daeb2d8492b16f
jessica <jessicayuhn@gmail.com>
2021-11-07 18:46:53 -0500

    bfs - Binary Tree Level Order Traversal

---
+++ 107.py
@@ -0,0 +1,18 @@
+from collections import deque
+def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+    if not root:
+        return []
+    q = deque([root])
+    res = []
+    while q:
+        level = []
+        for _ in range(len(q)):
+            node = q.popleft()
+            level.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        res.append(level)
+    res.reverse()
+    return res
