83be647d8b9a6d4dede9deafc206838c38cde97d
jessica <jessicayuhn@gmail.com>
2021-11-08 18:22:38 -0500

    dfs-BST range sum

---
+++ 938.py
@@ -0,0 +1,9 @@
+def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    if not root:
+        return 0
+    if root.val < low:
+        return  self.rangeSumBST(root.right, low, high)
+    elif root.val > high:
+        return  self.rangeSumBST(root.left, low, high)
+    else:
+        return root.val+self.rangeSumBST(root.left, low, high)+self.rangeSumBST(root.right,low,high)
