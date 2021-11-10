058016b1991d1bad4b4175e23f43fc6bceece122
jessica <jessicayuhn@gmail.com>
2021-11-10 11:04:13 -0500

    stack - Simplify Path

---
+++ 71.py
@@ -0,0 +1,10 @@
+def simplifyPath(self, path: str) -> str:
+    path = path.split('/')
+    stack = []
+    for c in path:
+        if c == '..':
+            if stack:
+                stack.pop()
+        elif c != '.' and c != '':
+            stack.append(c)
+    return '/' + '/'.join(stack)
