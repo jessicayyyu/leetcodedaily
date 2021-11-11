f4bc5ba887e241b43ac0e443c88639f28595d0da
jessica <jessicayuhn@gmail.com>
2021-11-11 16:05:00 -0500

    two pointers - reverse string

---
+++ 344.py
@@ -0,0 +1,9 @@
+def reverseString(self, s: List[str]) -> None:
+    if not s:
+        return []
+    left, right = 0, len(s) - 1
+    while left <= right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return s
