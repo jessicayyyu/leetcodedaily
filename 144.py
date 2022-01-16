c6c14c050d0e963606946c4c5c0bab140269c1dd
jessica <jessicayuhn@gmail.com>
2022-01-15 23:13:11 -0500

    DFS - Binary Tree Preorder Traversal

---
+++ 144.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        res = []
+        self.dfs(root, res)
+        return res
+
+    def dfs(self, root, res):
+        if not root:
+            return
+        res.append(root.val)
+        self.dfs(root.left, res)
+        self.dfs(root.right, res)
