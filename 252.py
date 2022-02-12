bf4212b534262c40caa428fc0d4ee41140cf700a
jessica <jessicayuhn@gmail.com>
2022-02-12 00:00:34 -0500

    Sweep Line - Meeting rooms

---
+++ 252.py
@@ -0,0 +1,8 @@
+def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+    if not intervals:
+        return True
+    intervals.sort(key=lambda x:x[0])
+    for i in range(len(intervals)-1):
+        if intervals[i+1][0] < intervals[i][1]:
+            return False
+    return True
