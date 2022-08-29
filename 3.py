bc16f3ed75bb7bae9a6d508370c096b7e2b9d048
jessica <jessicayuhn@gmail.com>
2022-08-29 13:44:45 -0400

    Sliding window - Longest Substring Without Repeating Characters

--- 3.py
+++ 3.py
@@ -1,12 +1,10 @@
 def lengthOfLongestSubstring(self, s: str) -> int:
-    i, j = 0, 0
-    unique_chars = set()
-    longest = 0
-    n = len(s)
-    for i in range(n):
-        while j < n and s[j] not in unique_chars:
-            unique_chars.add(s[j])
-            j += 1
-        longest = max(longest, j-i)
-        unique_chars.remove(s[i])
-    return longest
+    left, res = 0, 0
+    seen = {}
+    for right in range(len(s)):
+        c = s[right]
+        if c in seen:
+            left = max(left, seen.get(c))
+        seen[c] = right + 1 #left, right inclusive so add 1 to right to include it
+        res = max(res, right - left + 1)
+    return res
