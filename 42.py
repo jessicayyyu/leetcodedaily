ca02220c82dc64242fc80026e01d6a33f74d01c5
jessica <jessicayuhn@gmail.com>
2022-09-04 23:28:20 -0400

    Monotonic Stack - Trapping Rain Water

--- 42.py
+++ 42.py
@@ -1,22 +1,13 @@
 def trap(self, height: List[int]) -> int:
-    if not height:
-        return 0
-
-    left_max = []
-    cur_max = 0
-    for h in height:
-        cur_max = max(h, cur_max)
-        left_max.append(cur_max)
-
-    right_max = []
-    cur_max = 0
-    for h in reversed(height):
-        cur_max = max(cur_max, h)
-        right_max.append(cur_max)
-    right_max.reverse()
-
-    res = 0
+    res, stack = 0, []
     for i in range(len(height)):
-        res += min(left_max[i], right_max[i]) - height[i]
+        while stack and height[i] > height[stack[-1]]:
+            pre = stack.pop()
+            if not stack:
+                break
+            minHeight = min(height[i], height[stack[-1]])
+            res += (minHeight-height[pre])*(i-stack[-1]-1)
+        stack.append(i)
     return res
+
 
