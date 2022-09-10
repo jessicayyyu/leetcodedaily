3152f040d580a39ebf3a1814578808006e6df51a
jessica <jessicayuhn@gmail.com>
2022-09-10 00:39:53 -0400

    Monotonic deque - Sliding Window Maximum

--- 239.py
+++ 239.py
@@ -1,14 +1,16 @@
 from collections import deque
 def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-    q, res = deque([]), []
-    for i in range(len(nums)):
+    n = len(nums)
+    q = deque()
+    res = [0]*(n-k+1)
+    for i in range(n):
+        startwindowindex = i-k+1
+        while q and i-q[0] >= k:
+            q.popleft()
         while q and nums[i] >= nums[q[-1]]:
             q.pop()
         q.append(i)
-        if i + 1 >= k:
-            res.append(nums[q[0]])
-        if i + 1 - k == q[0]:
-            q.popleft()
+        if startwindowindex >= 0:
+            res[startwindowindex] = nums[q[0]]
     return res
-
 
